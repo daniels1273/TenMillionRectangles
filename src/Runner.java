21570acadec093d9441a21870ae597b49a1bfd8e
Daniel Sitnikov <dannycoolboy1324@gmail.com>
2026-03-17 20:58:22 -0400

    Finished part 2, can now generate 10 millions rectangles

--- src/Runner.java
+++ src/Runner.java
@@ -5,7 +5,8 @@ public class Runner {
         Point startPoint = new Point(10, 15);
         Rectangle originalRect = new Rectangle(20, 10, startPoint);
         ArrayList<Point> origPoints = originalRect.getAllPoints();
-        int counter = 0;
+        int counter1 = 0;
+        int counter2 = 0;
 
         for (int i = 0; i < 50; i++){
             ArrayList<Point> newPoints = Rectangle.generateRandomRectanglePoints();
@@ -13,7 +14,7 @@ public class Runner {
             for (Point point : origPoints){
                 for (Point newpoint : newPoints){
                     if (point.toString().equals(newpoint.toString())){
-                        counter++;
+                        counter1++;
                         shouldBreak = true;
                         break;
                     }
@@ -23,7 +24,22 @@ public class Runner {
                 }
             }
         }
-        System.out.println(counter);
+        System.out.println(counter1);
 
+        for (int i = 0; i < 10000000; i++){
+            Rectangle newRect = Rectangle.generateRandomRectangle();
+            ArrayList<Point> corners = newRect.findCorners();
+            for (Point corner : corners){
+                if (corner.getX() >= originalRect.getPoint().getX() && corner.getX() <= originalRect.getPoint().getX() + originalRect.getLength()){
+                    if (corner.getY() <= originalRect.getPoint().getY() && corner.getY() >= originalRect.getPoint().getY() - originalRect.getWidth()){
+                        counter2++;
+                        break;
+                    }
+                }
+            }
+        }
+        double percentTemp = (double) counter2 / 10000000;
+        int percent = (int) (percentTemp * 1000 + 0.5);
+        System.out.println(percent / 10.0 + "%");
     }
 }
